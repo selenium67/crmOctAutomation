93c1502f70a942a787df043c9c5eca0f92c5de13
selenium67 <seleniumjune18@gmail.com>
2018-10-31 08:29:01 +0530

    added xml and class files

---
+++ com.oct.project/src/test/java/tesgNGTestCases/SecondDemo.java
@@ -0,0 +1,21 @@
+package tesgNGTestCases;
+
+import org.testng.annotations.Test;
+
+public class SecondDemo {
+	
+	
+	@Test // @Test is equal to Main method in java
+	public void dample() {
+		
+		System.out.println("Hello TestNG");
+	}
+	
+	
+	@Test // @Test is equal to Main method in java
+	public void dummy() {
+		
+		System.out.println("Hello TestNG");
+	}
+
+}

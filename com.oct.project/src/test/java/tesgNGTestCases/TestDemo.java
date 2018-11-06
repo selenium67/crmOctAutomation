27659243b47dc1fac2fa453480cb1044e48759de
selenium67 <seleniumjune18@gmail.com>
2018-11-06 08:35:53 +0530

    sampath

--- com.oct.project/src/test/java/tesgNGTestCases/TestDemo.java
+++ com.oct.project/src/test/java/tesgNGTestCases/TestDemo.java
@@ -1,14 +1,40 @@
 package tesgNGTestCases;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class TestDemo {
+@Test
 
-	@Test
-	
-	public void hello()
+public class TestDemo {
 	{
 	
-	System.out.println("");
+	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Desktop\\downloads\\chromedriver_win32\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.osmania.ac.in");
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+		System.out.println(driver.getTitle());
+		
+		
+		
+		
+		//WebElement footer = driver.findElement(By.xpath("//section[@id='yt_spotlight8'"));
+	
+		
+		//System.out.println(footer.findElements(By.tagName("a")).size());
+		
+		
+		
+		
+		
+	
 	}
 }

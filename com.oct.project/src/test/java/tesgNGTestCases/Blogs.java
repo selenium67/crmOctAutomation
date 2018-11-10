e1be4df30ca08107dffbdac6c205fc64ff02dc83
selenium67 <seleniumjune18@gmail.com>
2018-11-10 22:43:46 +0530

    test case

--- com.oct.project/src/test/java/tesgNGTestCases/Blogs.java
+++ com.oct.project/src/test/java/tesgNGTestCases/Blogs.java
@@ -3,8 +3,10 @@ package tesgNGTestCases;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -19,7 +21,7 @@ import com.freecrm.webpages.LoginPOM;
 import sampath.CustomViews;
 
 public class Blogs extends CustomViews {
-	
+
 	public Properties prop = null;
 
 	public void getRepo() {
@@ -51,26 +53,47 @@ public class Blogs extends CustomViews {
 			Thread.sleep(3000);
 
 			driver.switchTo().frame(driver.findElement(By.xpath(getRepositoryData().getProperty("FREECRM.FRAME"))));
-			
+
 			driver.findElement(By.xpath("//a[@title='Blog']")).click();
-			
-			ArrayList<String> tab = new ArrayList<String>(driver.getWindowHandles());
-			
-			driver.switchTo().window(tab.get(1));
-			
-			List<WebElement> blogs = driver.findElements(By.xpath("//div[@id='post-listing']/article/h3/a"));
-			
-			for (WebElement ele : blogs) {
-				
-				System.out.println(ele.getText());
-				
-				if (ele.getText().equalsIgnoreCase("Managing the Customer Experience")) {
-					
-					ele.click();
-					break;
+
+			/*
+			 * ArrayList<String> tab = new ArrayList<String>(driver.getWindowHandles());
+			 * 
+			 * driver.switchTo().window(tab.get(1));
+			 */
+			String parent = driver.getWindowHandle();
+
+			Set<String> tabs = driver.getWindowHandles();
+
+			Iterator<String> ite = tabs.iterator();
+
+			while (ite.hasNext()) {
+
+				String child = ite.next();
+
+				if (!child.equals(parent)) {
+
+					driver.switchTo().window(child);
+
+					List<WebElement> blogs = driver.findElements(By.xpath("//div[@id='post-listing']/article/h3/a"));
+
+					for (WebElement ele : blogs) {
+
+						System.out.println(ele.getText());
+
+						if (ele.getText().equalsIgnoreCase("Managing the Customer Experience")) {
+
+							ele.click();
+							break;
+						}
+					}
+					driver.close();
 				}
 			}
-
+			
+			driver.switchTo().window(parent);
+			driver.switchTo().frame(driver.findElement(By.xpath(getRepositoryData().getProperty("FREECRM.FRAME"))));
+			driver.findElement(By.xpath("//a[@title='Knowledge Base']")).click();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
